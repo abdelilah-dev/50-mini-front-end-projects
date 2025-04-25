b768ae2a2f050b4ac68f73a38839ef15f5ae4607
AbdeLilah-Dev <contactme.abdelilah@gmail.com>
2025-04-25 17:50:37 +0100

    Upload the Update

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 let projectItems = document.querySelectorAll(".project-item");
 projectItems.forEach((ele, index) => {
     ele.style.backgroundImage = `url('./project-n${index + 1}/preview.jpg')`;
