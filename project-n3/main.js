048909c25c2a579e6c7ab062f5d6d2d51086769a
AbdeLilah-Dev <contactme.abdelilah@gmail.com>
2025-05-02 23:19:48 +0100

    Upload the Update

--- project-n3/main.js
+++ project-n3/main.js
@@ -1,10 +1,55 @@
-"use strict";
-let paperBtn = document.querySelector(".paper");
-let rockBtn = document.querySelector(".rock");
-let scissorsBtn = document.querySelector(".scissors");
-let choosenBtns = document.querySelectorAll(".btns > span");
-choosenBtns.forEach((ele) => {
-    ele.onclick = () => {
-        console.log(ele);
-    };
+var paperBtn = document.querySelector(".paper");
+var rockBtn = document.querySelector(".rock");
+var scissorsBtn = document.querySelector(".scissors");
+var gameResult = document.querySelector(".game-result");
+var totalScore = {
+    userScore: document.querySelector(".user-score"),
+    computerScore: document.querySelector(".computer-score"),
+};
+var choosenBtns = document.querySelectorAll(".btns > span");
+var choise;
+(function (choise) {
+    choise["rock"] = "rock";
+    choise["paper"] = "paper";
+    choise["scissors"] = "scissors";
+})(choise || (choise = {}));
+;
+var allGuess = [choise.rock, choise.paper, choise.scissors];
+choosenBtns.forEach(function (ele) {
+    ele.addEventListener("click", function (event) {
+        var target = event.target;
+        var playerChoice = target.className;
+        var computerChoice = allGuess[randomNum()];
+        checkTheWinner(playerChoice, computerChoice);
+    });
 });
+function checkTheWinner(playerChoice, computerChoice) {
+    var winner = "";
+    if (playerChoice) {
+        if ((playerChoice === "paper" && computerChoice === "rock") ||
+            (playerChoice === "scissors" && computerChoice === "paper") ||
+            (playerChoice === "rock" && computerChoice === "scissors")) {
+            winner = "player";
+        }
+        else {
+            winner = "computer";
+        }
+        updateResult(winner, playerChoice, computerChoice);
+        updateScore(winner);
+    }
+}
+function updateResult(winner, player, computer) {
+    if (winner === "player")
+        gameResult.innerHTML = "You Win! ".concat(player, " beats ").concat(computer);
+    else
+        gameResult.innerHTML = "You Lose! ".concat(computer, " beats ").concat(player);
+}
+function updateScore(winner) {
+    if (winner === "player")
+        totalScore.userScore.innerHTML = "".concat(+totalScore.userScore.innerHTML + 1);
+    else
+        totalScore.computerScore.innerHTML = "".concat(+totalScore.computerScore.innerHTML + 1);
+}
+function randomNum() {
+    return Math.floor(Math.random() * allGuess.length);
+}
--- project-n3/main.ts
+++ project-n3/main.ts
@@ -1,12 +1,53 @@
 let paperBtn = document.querySelector(".paper") as HTMLSpanElement;
 let rockBtn = document.querySelector(".rock") as HTMLSpanElement;
 let scissorsBtn = document.querySelector(".scissors") as HTMLSpanElement;
-
+let gameResult = document.querySelector(".game-result") as HTMLParagraphElement;
+let totalScore = {
+    userScore: document.querySelector(".user-score") as HTMLSpanElement,
+    computerScore: document.querySelector(".computer-score") as HTMLSpanElement,
+};
 let choosenBtns = document.querySelectorAll(".btns > span");
 
-choosenBtns.forEach((ele) => {
-    ele.onclick = () => {
-        console.log(ele);
-    }
+enum choise {
+    rock = "rock",
+    paper = "paper",
+    scissors = "scissors"
+};
 
+let allGuess: choise[] = [choise.rock, choise.paper, choise.scissors];
+
+choosenBtns.forEach((ele) => {
+    ele.addEventListener("click", event => {
+        let target = event.target as HTMLSpanElement;
+        let playerChoice: string = target.className;
+        let computerChoice: string = allGuess[randomNum()];
+        checkTheWinner(playerChoice, computerChoice);
+    })
 })
+
+function checkTheWinner(playerChoice: string, computerChoice: string): void {
+    let winner: string = "";
+    if (playerChoice) {
+        if ((playerChoice === "paper" && computerChoice === "rock") ||
+            (playerChoice === "scissors" && computerChoice === "paper") ||
+            (playerChoice === "rock" && computerChoice === "scissors")) {
+            winner = "player";
+        } else {
+            winner = "computer";
+        }
+        updateResult(winner, playerChoice, computerChoice);
+        updateScore(winner);
+    }
+}
+function updateResult(winner: string, player: string, computer: string) {
+    if (winner === "player") gameResult.innerHTML = `You Win! ${player} beats ${computer}`
+    else gameResult.innerHTML = `You Lose! ${computer} beats ${player}`
+}
+function updateScore(winner: string) {
+    if (winner === "player") totalScore.userScore.innerHTML = `${+totalScore.userScore.innerHTML + 1}`;
+    else totalScore.computerScore.innerHTML = `${+totalScore.computerScore.innerHTML + 1}`;
+}
+
+function randomNum(): number {
+    return Math.floor(Math.random() * allGuess.length);
+}
